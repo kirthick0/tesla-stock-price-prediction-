e31f268d787ec95ef57a2f999a403cdb9c4c6852
kirthick0 <kirthickpounraj@gmail.com>
2026-04-06 21:20:11 +0530

    Refactor Tesla data analysis and save cleaned data
    Added functionality to save cleaned data to a CSV file.

--- tesla.py
+++ tesla.py
@@ -1,31 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# Load dataset
 df = pd.read_csv("C:\Users\kirthick\Downloads\archive\TSLA.csv")
-
-# Convert Date column
 df['Date'] = pd.to_datetime(df['Date'])
-
-# Sort data
 df = df.sort_values(by='Date')
-
-# Show first rows
 print(df.head())
-
-# Basic info
 print(df.info())
-
-# Check missing values
 print(df.isnull().sum())
-
-# Create new column (Daily Change)
 df['Daily Change'] = df['Close'] - df['Open']
-
-# Summary statistics
 print(df.describe())
-
-# 📊 Plot Closing Price
 plt.figure()
 plt.plot(df['Date'], df['Close'])
 plt.title("Tesla Closing Price Over Time")
@@ -33,8 +15,6 @@ plt.xlabel("Date")
 plt.ylabel("Close Price")
 plt.xticks(rotation=45)
 plt.show()
-
-# 📊 Plot Volume
 plt.figure()
 plt.plot(df['Date'], df['Volume'])
 plt.title("Trading Volume Over Time")
@@ -42,6 +22,4 @@ plt.xlabel("Date")
 plt.ylabel("Volume")
 plt.xticks(rotation=45)
 plt.show()
-
-# Save cleaned data
 df.to_csv("cleaned_tsla.csv", index=False)
